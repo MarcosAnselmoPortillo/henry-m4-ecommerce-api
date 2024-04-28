848a47ec43ff68d21151bb9c7f25cdc1cbed72f0
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-04-28 15:25:22 -0300

    Updating update-user-dto

--- ecommerce-marcos-anselmo-portillo/src/users/dto/update-user.dto.ts
+++ ecommerce-marcos-anselmo-portillo/src/users/dto/update-user.dto.ts
@@ -2,36 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 import { IsEmail, IsInt, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsString()
-  @MinLength(3)
-  @MaxLength(50)
-  name: string;
-
-  @IsEmail()
-  @MaxLength(50)
-  email: string;
-
-  @IsString()
-  @MinLength(6)
-  @MaxLength(20)
-  password: string;
-
-  @IsInt()
-  @IsOptional()
-  phone?: number;
-
-  @IsString()
-  @IsOptional()
-  @MaxLength(50)
-  country?: string;
-
-  @IsString()
-  @IsOptional()
-  address?: string;
-
-  @IsString()
-  @IsOptional()
-  @MaxLength(50)
-  city?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}

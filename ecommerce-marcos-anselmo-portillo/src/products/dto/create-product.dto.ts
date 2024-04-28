81d427c0048fefd27a79f703dc00ad40b6888e1f
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-04-27 21:49:36 -0300

    Adding validations to create and update users and products DTOs. H3 - A3 - NestJS Routing - H4

--- ecommerce-marcos-anselmo-portillo/src/products/dto/create-product.dto.ts
+++ ecommerce-marcos-anselmo-portillo/src/products/dto/create-product.dto.ts
@@ -1,7 +1,27 @@
+import { IsString, IsNotEmpty, IsNumber, IsOptional, MaxLength, IsDecimal, Min } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
+  @IsDecimal({ decimal_digits: '2' })
+  @Min(0)
   price: number;
-  stock: boolean;
-  imgUrl: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  imgUrl?: string;
 }
--- ecommerce-marcos-anselmo-portillo/src/users/dto/create-user.dto.ts
+++ ecommerce-marcos-anselmo-portillo/src/users/dto/create-user.dto.ts
@@ -1,9 +1,35 @@
+import { IsEmail, IsString, IsOptional, MinLength, MaxLength, IsInt } from 'class-validator';
+
 export class CreateUserDto {
-  email: string;
+  @IsString()
+  @MinLength(3)
+  @MaxLength(50)
   name: string;
+
+  @IsEmail()
+  @MaxLength(50)
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
   password: string;
-  address: string;
-  phone: string;
+
+  @IsInt()
+  @IsOptional()
+  phone?: number;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(50)
   country?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(50)
   city?: string;
 }

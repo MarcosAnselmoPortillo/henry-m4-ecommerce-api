d0b9203add2925be1d8f9223706e6fe9fb8de7f9
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-05-02 09:27:30 -0300

    Updating sign-in.dto. H6 - A3 - NestJS Pipes - M4

--- ecommerce-marcos-anselmo-portillo/src/auth/dto/sign-in.dto.ts
+++ ecommerce-marcos-anselmo-portillo/src/auth/dto/sign-in.dto.ts
@@ -1,4 +1,7 @@
-export class SignInDto {
-    email: string;
-    password: string;
-  }
+import { PickType } from '@nestjs/mapped-types';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+export class SignInDto extends PickType(CreateUserDto, [
+  'email',
+  'password',
+] as const) {}

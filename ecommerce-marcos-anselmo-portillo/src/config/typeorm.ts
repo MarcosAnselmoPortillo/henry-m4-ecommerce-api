1579d5721622c3563ac8ce366ea9a2ee1431bd74
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-05-08 18:13:41 -0300

    Adding docker-compose configuration. H12 - Docker - M4

--- ecommerce-marcos-anselmo-portillo/src/config/typeorm.ts
+++ ecommerce-marcos-anselmo-portillo/src/config/typeorm.ts
@@ -1,19 +1,20 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-import {config as dotenvConfig} from 'dotenv';
+import { config as dotenvConfig } from 'dotenv';
 import { registerAs } from '@nestjs/config';
 
-dotenvConfig({path: '.env.development'});
+dotenvConfig({ path: '.env.development' });
 
 const config = {
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: 'postgresdb',
+  // host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*.{ts,js}'],
-  synchronize: true,
+  // synchronize: true,
   logging: true,
   // dropSchema: true,
 };
